da8d509d6d9b5b72fd08e84be9ac6329bb654c01
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-01-07 18:01:51 +0700

    initialize

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+api = Api(app)
+
+CORS(app)
+
+identity = {}
+
+class ExampleResource(Resource):
+    def get(self):
+        # response = {"msg" : "Hello this is restful API"}
+        return identity
+    
+    def post(self):
+        name = request.form["name"]
+        age = request.form["age"]
+        identity["name"] = name
+        identity["age"] = age
+        response = {"msg" : "Success insert data"}
+        return response
+
+    
+api.add_resource(ExampleResource, "/api", methods=["GET", "POST"])
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5005)
